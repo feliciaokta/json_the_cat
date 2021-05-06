a985ef77a5f84fd078dea54093be85f1407520ac
felicia puspita yuwono <feliciaokta@outlook.com>
2021-05-06 16:54:20 -0700

    finished assignment

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,40 @@
-const request = require("request"); // fetch Siberian data from API endpoint
-const breed = process.argv.slice(2)[0];
-const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+// const request = require("request"); // fetch Siberian data from API endpoint
+// const breed = process.argv.slice(2)[0];
+// const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-request(URL, function (error, response, body) {
-  if (error) {
-    return console.log(error);
-  }
-  if (response === []) {
-    return console.log("Breed not found");
-  }
-  if (response) {
-    const data = JSON.parse(body);
-    console.log(data[0]["description"]);
-  }
-})
+
+// // https://github.com/request/request#readme
+// // read section "Super simple to use"
+
+// request(URL, function (error, response, body) {
+//   if (error) {
+//     return console.log(error);
+//   }
+//   if (response === []) {
+//     return console.log("Breed not found");
+//   }
+//   if (response) {
+//     const data = JSON.parse(body);
+//     console.log(data[0]["description"]);
+//   }
+// });
+
+const request = require("request"); // fetch Siberian data from API endpoint
+
+const fetchBreedDescription = function(breedName, callback) {
+  const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(URL, function (error, response, body) {
+    if (error) {
+      callback(error, null);
+    } else if (response) {
+      const data = JSON.parse(body);
+      callback(null, data[0].description);
+    }
+  })
+};
+
+// breedName = tabbycat
+// fetchBreedDescription(tabbycat, callback)
+// callback (data) => {console.log(data of tabbycat)}
+
+module.exports = { fetchBreedDescription };
---
+++ index.js
@@ -0,0 +1,11 @@
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv[2];
+const callback = (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+}
+fetchBreedDescription(breedName, callback);
